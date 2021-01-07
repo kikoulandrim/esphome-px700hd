ab83ddc50e06fce1424dd861647d0f2b07dde44a
kikoulandrim <50570400+kikoulandrim@users.noreply.github.com>
2021-01-07 02:39:31 +0100

    Update uart_read_px700hd.h

--- uart_read_px700hd.h
+++ uart_read_px700hd.h
@@ -1,9 +1,9 @@
 class px700hdSensor : public PollingComponent,  public UARTDevice, public sensor::Sensor{
  public:
-  px700hdSensor(UARTComponent *parent) : PollingComponent(10000), UARTDevice(parent) {}
+  px700hdSensor(UARTComponent *parent) : PollingComponent(1000), UARTDevice(parent) {}
 
-  byte last[9];
-  float value = 3;
+  byte last[2];
+  float value = 2;
   float lastPublished = -1;
 
   void setup() override {
@@ -13,22 +13,18 @@ class px700hdSensor : public PollingComponent,  public UARTDevice, public sensor
   void loop() override {
     while (available()) {
       byte in = read();
-      if(last[8] == 0x05 && last[7] == 0x14 && last[6] == 0x00 && last[5] == 0x03 && last[4] == 0x00 && last[3] == 0x00 && last[2] == 0x00 && last[1] == 0x01 && last[0] == 0x18){
+      if(last[1] == 0x01 && last[0] == 0x18){
         value = 1;
       }
-      else if(last[8] == 0x05 && last[7] == 0x14 && last[6] == 0x00 && last[5] == 0x03 && last[4] == 0x00 && last[3] == 0x00 && last[2] == 0x00 && last[1] == 0x00 && last[0] == 0x17){
+      else if(last[1] == 0x00 && last[0] == 0x17){
         value = 0;
+      }
+      else if(last[1] == 0x00 && last[0] == 0x14){
+        value = 2;
       }
 //      else {
 //       value = 2;
 //      }
-     last[8] = last[7];
-     last[7] = last[6];
-     last[6] = last[5];
-     last[5] = last[4];
-     last[4] = last[3];
-     last[3] = last[2];
-     last[2] = last[1];
      last[1] = last[0];
      last[0] = in;
      yield();
